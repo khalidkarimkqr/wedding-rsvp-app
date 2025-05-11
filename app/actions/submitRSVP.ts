bd7e123d674b8fa3f6f50fe603499d8e8e1da66f
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-10 23:22:16 -0700

    implementing the authentication and owner access to admin page

--- app/actions/submitRSVP.ts
+++ app/actions/submitRSVP.ts
@@ -2,9 +2,9 @@
 
 import { strings } from "../utils/strings";
 import { createClient } from "../utils/supabase/server";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function submitRSVP(formData: FormData) {
   const supabase = await createClient();
@@ -24,9 +24,29 @@ export async function submitRSVP(formData: FormData) {
     return { success: false, message: "Failed to submit RSVP", error };
   }
 
-  // Send email notification
-  if (!strings.sendToEmail) {
-    console.error("No email to send to");
-    return { success: false, message: "No email to send to" };
-  }
-  
+//   // Send email notification
+//   if (!strings.sendToEmail) {
+//     console.error("No email to send to");
+//     return { success: false, message: "No email to send to" };
+//   }
+//   if (!error) {
+//     try {
+//       await resend.emails.send({
+//         from: "RSVP <onboarding@resend.dev>",
+//         to: strings.sendToEmail,
+//         subject: "New RSVP Submission",
+//         html: `
+//         <h1>New RSVP Submission</h1>
+//         <p><strong>Name:</strong> ${name}</p>
+//         <p><strong>Email:</strong> ${email}</p>
+//         <p><strong>Number of Guests:</strong> ${accompany}</p>
+//         <p><strong>Attendance:</strong> ${attendance}</p>
+//       `,
+//       });
+//     } catch (error) {
+//       console.error("Error sending email:", error);
+//     }
+//   }
+
+  return { success: true, message: "RSVP submitted successfully" };
+}
